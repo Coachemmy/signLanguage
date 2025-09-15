23e69042a8d429cc317b9fdec78df7aaecb23595
CoachEmmy <coachemmyb@gmail.com>
2025-09-16 00:30:40 +0800

    update dataTest for fingers A-F

--- dataTest.py
+++ dataTest.py
@@ -2,13 +2,16 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import  numpy as np
 import math
-from streamlit import success
+import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
 
 offset = 20
 imgSize = 300
+
+folder = "Data/F"
+counter = 0
 
 while True:
     success, img = cap.read()
@@ -29,9 +32,8 @@ while True:
             wCal = math.ceil(k * w)
             imgResize = cv2.resize(imgCrop, (wCal, imgSize))
             imgResizeShape = imgResize.shape
-            wGap = math.ceil((imgSize - wCal)/2)
+            wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
-
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
@@ -41,7 +43,11 @@ while True:
             imgWhite[hGap:hCal + hGap, :] = imgResize
 
         cv2.imshow("ImageCrop", imgCrop)
-        cv2.imshow("ImageWhite", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
