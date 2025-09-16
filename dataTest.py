c3463d406415fcadfdd5985d39220e86856bd5d2
CoachEmmy <coachemmyb@gmail.com>
2025-09-17 04:23:22 +0800

    updating testing file

--- dataTest.py
+++ dataTest.py
@@ -1,11 +1,13 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+from cvzone.ClassificationModule import Classifier
 import  numpy as np
 import math
 import time
 
 cap = cv2.VideoCapture(0)
 detector = HandDetector(maxHands=1)
+classifier = Classifier("Model/keras_model.h5","`Model/labels.txt")
 
 offset = 20
 imgSize = 300
@@ -13,19 +15,22 @@ imgSize = 300
 folder = "Data/F"
 counter = 0
 
+labels = ["A","B","C","D","E","F"]
+
 while True:
     success, img = cap.read()
+    imgNew = img.copy()
     hands, img = detector.findHands(img)
     if hands:
         hand = hands[0]
-        x,y,w,h = hand['bbox']
+        x, y, w, h = hand['bbox']
 
         imgWhite = np.ones((imgSize, imgSize, 3), np.uint8)*255
         imgCrop = img[y - offset:y + h + offset, x - offset:x + w + offset]
 
         imgCropShape = imgCrop.shape
 
-        aspectRatio = h/w
+        aspectRatio = h / w
 
         if aspectRatio > 1:
             k = imgSize / h
@@ -34,6 +39,10 @@ while True:
             imgResizeShape = imgResize.shape
             wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+            print(prediction, index)
+
+
         else:
             k = imgSize / w
             hCal = math.ceil(k * h)
@@ -41,10 +50,14 @@ while True:
             imgResizeShape = imgResize.shape
             hGap = math.ceil((imgSize - hCal) / 2)
             imgWhite[hGap:hCal + hGap, :] = imgResize
+            prediction, index = classifier.getPrediction(imgWhite, draw=False)
+
+
+        cv2.putText(imgNew, labels[index],(x, y - 25), cv2.FONT_ITALIC, 2,(255, 0, 255), 4)
 
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
-    cv2.imshow("Image", img)
+    cv2.imshow("Image", imgNew)
     cv2.waitKey(1)
 
